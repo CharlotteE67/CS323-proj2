2bb4d14f6e7e4621b2b87f8cf59085d8cfcb3abb
CharlotteE67 <11812419@mail.sustech.edu.cn>
2021-11-07 11:39:21 +0800

    initial spl type

---
+++ proj1/spl_type.cpp
@@ -0,0 +1,42 @@
+#include "type.hpp"
+
+Type::Type(string _name, CATEGORY _category, dTypes _type):
+        name(std::move(_name)),
+        category(_category),
+        type(std::move(_type)) {
+}
+
+#define getPrimitive(TYPE) \
+Type *Type::getPrimitive##TYPE() { \
+    return new Type{#TYPE, CATEGORY::PRIMITIVE, Node_TYPE::TYPE};\
+}
+
+getPrimitive(INT);
+
+getPrimitive(CHAR);
+
+getPrimitive(FLOAT);
+
+#undef getPrimitive
+
+Type *Type::getPrimitiveType(Node_TYPE type) {
+    switch (type) {
+        case Node_TYPE::INT : {
+            return Type::getPrimitiveINT();
+        }
+        case Node_TYPE::FLOAT : {
+            return Type::getPrimitiveFLOAT();
+        }
+        case Node_TYPE::CHAR : {
+            return Type::getPrimitiveCHAR();
+        }
+        default: {
+        }
+    }
+}
+
+Array::Array(Type *base, int size) : base(base), size(size) {}
+
+FieldList::FieldList(string name, Type *type, FieldList *next) : name(std::move(name)), type(type), next(next) {}
+
+
---
+++ proj1/spl_type.hpp
@@ -0,0 +1,59 @@
+#ifndef SPL_TYPE_HPP
+#define SPL_TYPE_HPP
+
+#include <string>
+#include <vector>
+#include "spl_node.hpp"
+
+using namespace std;
+
+enum class CATEGORY {
+    PRIMITIVE, ARRAY, STRUCTURE, FUNCTION
+};
+
+union class dTypes {
+    Node_TYPE, Array*, FieldList*
+};
+
+class Array;
+
+class FieldList;
+
+class Type {
+public:
+    string name;
+    CATEGORY category = CATEGORY::PRIMITIVE;
+    dTypes type
+    Type *returnType = nullptr; // 存储返回值的类型
+
+    static Type *getPrimitiveType(Node_TYPE type);
+    static Type *getPrimitiveINT();
+    static Type *getPrimitiveFLOAT();
+    static Type *getPrimitiveCHAR();
+
+    Type() = default;
+    Type(string _name, CATEGORY _category, dTypes _type);
+
+};
+
+class Array {
+public:
+    Type *base;
+    int size;
+
+    Array(Type *base, int size);
+};
+
+class FieldList {
+public:
+    string name;
+    Type *type = nullptr;
+    FieldList *next = nullptr;
+
+    FieldList() = default;
+
+    FieldList(string name, Type *type, FieldList *next);
+
+};
+
+#endif
